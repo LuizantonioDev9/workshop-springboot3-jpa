b59a33bad2433f24cf005729d9f52158f6dc7845
LuizantonioDev9 <luizantoniodev9@gmail.com>
2025-01-27 10:55:16 -0300

    Project created

---
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -0,0 +1,11 @@
+package com.educandoweb.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication//anotação para que ele seja uma aplição do springboot
+public class CourseApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+}
